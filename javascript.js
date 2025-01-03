1934324622ab1ab7e197aea30a01570d3573600d
WillRB01 <191542342+WillRB01@users.noreply.github.com>
2025-01-03 15:53:33 -0800

    Implement play round game logic
    Games don't go on forever - someone has to win and someone has to lose!
    Rounds and increasing scores help determine who's winning and when one
    game ends and when a new game begins.
    
    The game had no round-based system or score increasing system before
    this commit.
    
    I went ahead and used switch cases to handle the logic in this section
    of the game as it seems to handle the game's needs quite effectively and
    is better than having a bunch of if..else conditionals for each outcome.
    
    And in order to make the game's code neater, I renamed, added, cut out,
    and shuffled around bits of code for better readability and to reduce
    scope as to not evoke certain functions more than once.

--- javascript.js
+++ javascript.js
@@ -3,36 +3,95 @@
 let humanScore = 0;
 let computerScore = 0;
 
-// In-game computer game logic
-
-let rand = Math.random();
-
-function getComputerChoice() {
-    let num = Math.floor(rand * 12) + 1;
-    if (num <= 4) {
-        return 'Rock';
-    } else if (num > 4 && num <= 8){
-        return 'Paper';
-    } else {
-        return 'Scissors';
-    }
-}
-
-console.log(getComputerChoice());
-
 // Player game logic
 
 function getHumanChoice() {
-    let humanChoice = prompt('Make your choice!').toLowerCase();
-    if (humanChoice === 'rock') {
+    let humanInput = prompt('Make your choice!').toLowerCase();
+    if (humanInput === 'rock') {
         return 'Rock';
-    } else if(humanChoice === 'paper') {
+    } else if(humanInput === 'paper') {
         return 'Paper';
-    } else if (humanChoice === 'scissors') {
+    } else if (humanInput === 'scissors') {
         return 'Scissors';
     } else {
-        return 'Invalid choice... Please try again.'
+        return 'Invalid choice... Please try again.';
     }
 }
 
-console.log(getHumanChoice());
+// In-game computer game logic
+
+function getComputerChoice() {
+    let computerOutput = Math.floor(Math.random() * 12) + 1;
+    if (computerOutput <= 4) {
+        return 'Rock';
+    } else if (computerOutput >= 4 && computerOutput < 8) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+// Play round game logic
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    switch(true) {
+        case (humanSelection === 'Rock' && computerSelection === 'Scissors') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You win! Rock beats scissors.');
+            return humanScore += 1;
+            break;
+        case (humanSelection === 'Rock' && computerSelection === 'Paper') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You lose! Paper beats rock.');
+            return computerScore += 1;
+            break;
+        case (humanSelection === 'Rock' && computerSelection === 'Rock') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('It\s a draw! Try again. :)');
+            break;
+        case (humanSelection === 'Paper' && computerSelection === 'Rock') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You win! Paper beats rock.');
+            return humanScore += 1;
+            break;
+        case (humanSelection === 'Paper' && computerSelection === 'Scissors'):
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You lose! Scissors beats paper.');
+            return computerScore += 1;
+            break;
+        case (humanSelection === 'Paper' && computerSelection === 'Paper') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('It\s a draw! Try again. :)');
+            break;
+        case (humanSelection === 'Scissors' && computerSelection === 'Paper') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You win! Scissors beats paper.');
+            return humanScore += 1;
+            break;
+        case (humanSelection === 'Scissors' && computerSelection === 'Rock'):
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('You lose! Rock beats scissors.');
+            return computerScore += 1;
+            break;
+        case (humanSelection === 'Scissors' && computerSelection === 'Scissors') :
+            console.log(humanSelection);
+            console.log(computerSelection);
+            console.log('It\s a draw! Try again. :)');
+            break;
+        default: 
+        console.log('Not an option... Please try again.');
+    }
+}
+
+playRound(humanSelection, computerSelection);
