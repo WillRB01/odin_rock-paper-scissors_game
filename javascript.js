d666b72fd9111e8bc6e2a672b18b996f12f119a0
WillRB01 <191542342+WillRB01@users.noreply.github.com>
2025-01-04 19:35:18 -0800

    Bug #1 and #2 fixed - game is now functional!
    The game now works exactly as intended - classic rock, paper, scissors
    game where first player to 5 - wins.
    
    getHumanChoice() and getComputerChoice() are now INSIDE playGame() and
    more importantly, INSIDE of the round logic for loop - meaning that every
    time the for loop's parameters aren't met, it loops and both getHumanChoice()
    and getComputerChoice() get evoked as if it were the first time they were
    being called upon. Clean slates for both every round.
    
    And given that they are both in the round logic for loop, they stop exactly
    where they need to - when someone scores 5 points.
    
    I also removed the getHumanChoice() and getComputerChoice() within playRound()
    as they aren't needed there and caused the getHumanChoice() prompt to
    show up twice per round.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,4 @@
-// Player game logic
-function getHumanChoice() {
-    let humanInput = prompt('Make your choice!').toLowerCase();
-    if (humanInput === 'rock') {
-        return 'Rock';
-    } else if(humanInput === 'paper') {
-        return 'Paper';
-    } else if (humanInput === 'scissors') {
-        return 'Scissors';
-    } else {
-        return 'Invalid choice... Please try again.';
-    }
-}
-
-// In-game computer game logic
-function getComputerChoice() {
-    let computerOutput = Math.floor(Math.random() * 12) + 1;
-    if (computerOutput <= 4) {
-        return 'Rock';
-    } else if (computerOutput >= 4 && computerOutput < 8) {
-        return 'Paper';
-    } else {
-        return 'Scissors';
-    }
-}
-
-// Play game, game logic
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
+//Rock, paper, scissors game logic!
 function playGame() {
 
     //Initialize scores, number of maximum rounds and winning score
@@ -39,7 +10,7 @@ function playGame() {
     //Round logic
     for (let round = 1; round <= maxRounds; round++) {
 
-        //Score checker
+        //Score checker logic
         if (humanScore === winningScore) {
             console.log('You win the game!')
             break;
@@ -48,7 +19,37 @@ function playGame() {
             break;
         }
 
-        // Play round game logic
+        //Player game logic
+        function getHumanChoice() {
+            let humanInput = prompt('Make your choice!').toLowerCase();
+            if (humanInput === 'rock') {
+                return 'Rock';
+            } else if(humanInput === 'paper') {
+                return 'Paper';
+            } else if (humanInput === 'scissors') {
+                return 'Scissors';
+            } else {
+                return 'Invalid choice... Please try again.';
+            }
+        }
+
+        //In-game computer game logic
+        function getComputerChoice() {
+            let computerOutput = Math.floor(Math.random() * 12) + 1;
+            if (computerOutput <= 4) {
+                return 'Rock';
+            } else if (computerOutput >= 4 && computerOutput < 8) {
+                return 'Paper';
+            } else {
+                return 'Scissors';
+            }
+        }
+
+        //Play game, game logic
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        //Play round game logic
         function playRound(humanChoice, computerChoice) {
             switch(true) {
                 case (humanSelection === 'Rock' && computerSelection === 'Scissors') :
@@ -57,7 +58,6 @@ function playGame() {
                     console.log('You win! Rock beats scissors.');
                     ++humanScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Rock' && computerSelection === 'Paper') :
                     console.log(humanSelection);
@@ -65,13 +65,11 @@ function playGame() {
                     console.log('You lose! Paper beats rock.');
                     ++computerScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Rock' && computerSelection === 'Rock') :
                     console.log(humanSelection);
                     console.log(computerSelection);
                     console.log('It\s a draw! Try again. :)');
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Paper' && computerSelection === 'Rock') :
                     console.log(humanSelection);
@@ -79,7 +77,6 @@ function playGame() {
                     console.log('You win! Paper beats rock.');
                     ++humanScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Paper' && computerSelection === 'Scissors'):
                     console.log(humanSelection);
@@ -87,13 +84,11 @@ function playGame() {
                     console.log('You lose! Scissors beats paper.');
                     ++computerScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Paper' && computerSelection === 'Paper') :
                     console.log(humanSelection);
                     console.log(computerSelection);
                     console.log('It\s a draw! Try again. :)');
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Scissors' && computerSelection === 'Paper') :
                     console.log(humanSelection);
@@ -101,7 +96,6 @@ function playGame() {
                     console.log('You win! Scissors beats paper.');
                     ++humanScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Scissors' && computerSelection === 'Rock'):
                     console.log(humanSelection);
@@ -109,13 +103,11 @@ function playGame() {
                     console.log('You lose! Rock beats scissors.');
                     ++computerScore;
                     console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    getHumanChoice(); getComputerChoice();
                     break;
                 case (humanSelection === 'Scissors' && computerSelection === 'Scissors') :
                     console.log(humanSelection);
                     console.log(computerSelection);
                     console.log('It\s a draw! Try again. :)');
-                    getHumanChoice(); getComputerChoice();
                     break;
                 default: 
                 console.log('Not an option... Please try again.');
@@ -123,6 +115,7 @@ function playGame() {
         }
 
         playRound(humanSelection, computerSelection);
+        
     }
 }
 
