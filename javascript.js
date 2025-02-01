6913a5e42b1d61ecf485442ac6630bd561f2c97e
WillRB01 <191542342+WillRB01@users.noreply.github.com>
2025-02-01 13:27:45 -0800

    Refactor both in-game player's logic
    The previous version of the game was too long and clunky.
    
    Starting with both of the player's getting functions - they've have rewritten
    to shorten overall code length and better fit the need of the new code's structure.
    
    I decided to make GetComputerChoice() use an array tied in with a random number generator
    that then serves to return one of the 3 choices as it considerably reduces the lines of code
    needed.
    
    Both functions now also return lowercased strings as they better fit the overall needs of the
    game later down the line.

--- javascript.js
+++ javascript.js
@@ -1,122 +1,21 @@
-//Rock, paper, scissors game logic!
-function playGame() {
+//In-game computer game logic
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * choices.length);
+    return choices[randomNum];
+};
 
-    //Initialize scores, number of maximum rounds and winning score
-    let humanScore = 0;
-    let computerScore = 0;
-    const winningScore = 5;
-    const maxRounds = 20;
-
-    //Round logic
-    for (let round = 1; round <= maxRounds; round++) {
-
-        //Score checker logic
-        if (humanScore === winningScore) {
-            console.log('You win the game!')
-            break;
-        } else if (computerScore === winningScore) {
-            console.log('You lost... Computer wins the game!')
-            break;
-        }
-
-        //Player game logic
-        function getHumanChoice() {
-            let humanInput = prompt('Make your choice!').toLowerCase();
-            if (humanInput === 'rock') {
-                return 'Rock';
-            } else if(humanInput === 'paper') {
-                return 'Paper';
-            } else if (humanInput === 'scissors') {
-                return 'Scissors';
-            } else {
-                return 'Invalid choice... Please try again.';
-            }
-        }
-
-        //In-game computer game logic
-        function getComputerChoice() {
-            let computerOutput = Math.floor(Math.random() * 12) + 1;
-            if (computerOutput <= 4) {
-                return 'Rock';
-            } else if (computerOutput >= 4 && computerOutput < 8) {
-                return 'Paper';
-            } else {
-                return 'Scissors';
-            }
-        }
-
-        //Play game, game logic
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        //Play round game logic
-        function playRound(humanChoice, computerChoice) {
-            switch(true) {
-                case (humanSelection === 'Rock' && computerSelection === 'Scissors') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You win! Rock beats scissors.');
-                    ++humanScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Rock' && computerSelection === 'Paper') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You lose! Paper beats rock.');
-                    ++computerScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Rock' && computerSelection === 'Rock') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('It\s a draw! Try again. :)');
-                    break;
-                case (humanSelection === 'Paper' && computerSelection === 'Rock') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You win! Paper beats rock.');
-                    ++humanScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Paper' && computerSelection === 'Scissors'):
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You lose! Scissors beats paper.');
-                    ++computerScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Paper' && computerSelection === 'Paper') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('It\s a draw! Try again. :)');
-                    break;
-                case (humanSelection === 'Scissors' && computerSelection === 'Paper') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You win! Scissors beats paper.');
-                    ++humanScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Scissors' && computerSelection === 'Rock'):
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('You lose! Rock beats scissors.');
-                    ++computerScore;
-                    console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
-                    break;
-                case (humanSelection === 'Scissors' && computerSelection === 'Scissors') :
-                    console.log(humanSelection);
-                    console.log(computerSelection);
-                    console.log('It\s a draw! Try again. :)');
-                    break;
-                default: 
-                console.log('Not an option... Please try again.');
-            }
-        }
-
-        playRound(humanSelection, computerSelection);
-        
-    }
-}
-
-playGame();
+//Player game logic
+function getHumanChoice() {
+    const humanChoice = prompt('Enter your choice: rock, paper, or scissors').toLowerCase();
+    if (humanChoice === 'rock') {
+        return 'rock';
+    } else if(humanChoice === 'paper') {
+        return 'paper';
+    } else if (humanChoice === 'scissors') {
+        return 'scissors';
+    } else {
+        console.log('Invalid choice... please enter rock, paper, or scissors.');
+        return getHumanChoice();
+    };
+};
