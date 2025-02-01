ac8dc50e16af2bacd60d870feca27fb6e8ecd1a2
WillRB01 <191542342+WillRB01@users.noreply.github.com>
2025-02-01 14:10:19 -0800

    Implement refactored playGame() and playRound()
    Both functions have been refactored to use far less code.
    
    playRound() has been chopped by more than half in terms of code required to execute
    and still works as intended - if not better then before.
    
    PlayGame()'s scope has now also been reduced to only include what it needs to instead
    of the entirety of the code.

--- javascript.js
+++ javascript.js
@@ -19,3 +19,36 @@ function getHumanChoice() {
         return getHumanChoice();
     };
 };
+
+function playGame() {
+    //Initialize player scores, the max number of rounds & the game winning score
+    let humanScore = 0;
+    let computerScore = 0;
+    const winningScore = 5;
+    const maxRounds = 20;
+
+    //Play round logic
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            console.log('You win! Rock beats scissors.');
+            ++humanScore;
+            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            console.log('You win! Paper beats rock.');
+            ++humanScore;
+            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            console.log('You win! Scissors beats paper.');
+            ++humanScore;
+            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+        } else if (humanChoice === computerChoice) {
+            console.log('It\'s a draw! Try again. :)');
+        } else {
+            console.log(`You lose... ${computerChoice} beats ${humanChoice}.`);
+            ++computerScore;
+            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+        };
+    };
+};
+
+playGame();
