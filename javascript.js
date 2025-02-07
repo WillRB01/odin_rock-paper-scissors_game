352aece39a0ea60b494dd128ac1bea367e9b6a87
WillRB01 <191542342+WillRB01@users.noreply.github.com>
2025-02-06 18:02:56 -0800

    Refactor playGame() and playRound()
    PlayGame() and playRound() both needed to be refactored to fit the new
    requirements of the code.
    
    playGame() now encapsulates all of the currently existing code. I did this
    because the buttons that now serve as the human player's way of inputting
    their choice needs access to playRound() to work properly.
    
    playRound() now also uses the DOM to add the appropriate text to the .display-area div.
    
    Note - the game still does not work as intended.

--- javascript.js
+++ javascript.js
@@ -1,65 +1,66 @@
-//In-game computer game logic
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomNum = Math.floor(Math.random() * choices.length);
-    return choices[randomNum];
-};
-
-//Player game logic
-const rockBtn = document.querySelector('.rockBtn');
-rockBtn.addEventListener('click', () => {
-    const humanSelection = 'rock';
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-});
-
-const paperBtn = document.querySelector('.paperBtn');
-paperBtn.addEventListener('click', () => {
-    const humanSelection = 'paper';
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-});
-
-const scissorsBtn = document.querySelector('.scissorsBtn');
-scissorsBtn.addEventListener('click', () => {
-    const humanSelection = 'scissors';
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
-});
-
 function playGame() {
     //Initialize player scores, the max number of rounds & the game winning score
     let humanScore = 0;
     let computerScore = 0;
     const winningScore = 5;
-    const maxRounds = 20;
+
+    //In-game computer game logic
+    function getComputerChoice() {
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomNum = Math.floor(Math.random() * choices.length);
+        return choices[randomNum];
+    };
+
+    //Player game logic
+    const displayArea = document.querySelector('.display-area');
+    displayArea.style.whiteSpace = "pre-line;"
+
+    const rockBtn = document.querySelector('.rockBtn');
+    rockBtn.addEventListener('click', () => {
+        const humanSelection = 'rock';
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    });
+
+    const paperBtn = document.querySelector('.paperBtn');
+    paperBtn.addEventListener('click', () => {
+        const humanSelection = 'paper';
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    });
+
+    const scissorsBtn = document.querySelector('.scissorsBtn');
+    scissorsBtn.addEventListener('click', () => {
+        const humanSelection = 'scissors';
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    });
 
     //Play round logic
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            console.log('You win! Rock beats scissors.');
+            displayArea.textContent = 'You win! Rock beats scissors.' + '\n';
             ++humanScore;
-            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+            displayArea.textContent += `Me: ${humanScore} - Computer: ${computerScore}`;
         } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-            console.log('You win! Paper beats rock.');
+            displayArea.textContent = 'You win! Paper beats rock.';
             ++humanScore;
-            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+            displayArea.textContent += `Me: ${humanScore} - Computer: ${computerScore}`;
         } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            console.log('You win! Scissors beats paper.');
+            displayArea.textContent = 'You win! Scissors beats paper.' + '\n';
             ++humanScore;
-            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+            displayArea.textContent += `Me: ${humanScore} - Computer: ${computerScore}`;
         } else if (humanChoice === computerChoice) {
-            console.log('It\'s a draw! Try again. :)');
+            displayArea.textContent = 'It\'s a draw! Try again. :)';
         } else {
-            console.log(`You lose... ${computerChoice} beats ${humanChoice}.`);
+            displayArea.textContent = `You lose... ${computerChoice} beats ${humanChoice}.` + '\n';
             ++computerScore;
-            console.log(`Me: ${humanScore} - Computer: ${computerScore}`);
+            displayArea.textContent += `Me: ${humanScore} - Computer: ${computerScore}`;
         };
     };
 };
 
 playGame();
-
